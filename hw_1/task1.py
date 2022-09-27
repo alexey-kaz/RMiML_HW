052fce2386ebef05adc1011f327e7ce2c968f3b7
Alexey Kazantaev <akazantaev@viju.ru>
2022-09-27 20:37:05 +0300

    HW1

--- hw_1/task1.py
+++ hw_1/task1.py
@@ -1,8 +1,7 @@
 def balanced_brackets(st):
     d = dict(zip('({[', ')}]'))
     stack = []
-    st1 = ''.join([i for i in st if i in d.keys() | d.values()])
-    for char in st1:
+    for char in st:
         if char in d.keys():
             stack.append(d[char])
         elif char in d.values():
